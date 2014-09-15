b6cbc5533647d1eb268f1f99a99ea5ded4d69a16
Raphaël Londeix <raphael.londeix@infinit.io>
2014-09-15 23:42:04 +0200

    ll

--- src/mail/commands/show.py
+++ src/mail/commands/show.py
@@ -6,6 +6,10 @@ import colorama
 import blessings
 import textwrap
 import fabulous
+import requests
+from fabulous import image
+import fabulous.color
+from io import BytesIO
 
 from mail import message, db, account, object
 
@@ -26,6 +30,8 @@ class TerminalRenderer(mistune.Renderer):
         self.indent = 0
         super().__init__()
         self.term = blessings.Terminal(force_styling = True)
+        self.old_text = False
+        self.is_image = False
 
     def link(self, link, title, content):
         res = title and title + ": " or ''
@@ -33,26 +39,41 @@ class TerminalRenderer(mistune.Renderer):
         return res
 
     def paragraph(self, txt):
+        if self.is_image:
+            self.is_image = False
+            return txt
+
+        if not txt: return ''
         res = []
-        lines = textwrap.wrap(txt, width = 79)
+        lines = textwrap.wrap(txt, width = 71)
         for line in lines[:-1]:
             words = line.split()
             spaces = (len(words) - 1) * [' ']
             words_length = sum(len(w) for w in words)
-            spaces_left = (80 - len(line))
+            spaces_left = (72 - len(line))
             word_weights = [
                 (len(word), pos) for pos, word in enumerate(words[1:])
             ]
-            while spaces_left and word_weights:
-                for w, i in reversed(sorted(word_weights)):
-                    if not spaces_left: break
-                    spaces[i] += ' '
-                    spaces_left -= 1
+            if spaces_left < words_length / 2:
+                while spaces_left and word_weights:
+                    for w, i in reversed(sorted(word_weights)):
+                        if not spaces_left: break
+                        spaces[i] += ' '
+                        spaces_left -= 1
             parts = map(lambda e: ''.join(e), itertools.zip_longest(spaces, words[1:]))
             res.append(words[0] + ''.join(parts))
         if lines:
             res.append(lines[-1])
-        return '\n' +'\n'.join(res) + '\n'
+        if res == ['&gt;']:
+            self.old_text = True
+            return ''
+
+        if self.old_text:
+            self.old_text = False
+            res = fabulous.color.bg256("#111", '    ') + ('\n' + str(fabulous.color.bg256("#111", '    '))).join(res) + '\n'
+            return  fabulous.color.bg256("#111", '    \n')+ fabulous.color.fg256("#aaa", res)
+        res = '    ' + ('\n' + '    ').join(res) + '\n'
+        return '\n'+res
 
     def list(self, body, ordered = True):
         self.indent = 2
@@ -76,29 +97,39 @@ class TerminalRenderer(mistune.Renderer):
     def autolink(self, link, is_email = False):
         import re
         link = link.replace('\n', '')
-        print(link)
         s = re.search('\[(.*)\]', link)
         if s:
             return self.term.blue(s.group(1))
         else:
             return self.term.italic(self.term.blue(link))
 
+    has_newline = False
+    def text(self, text):
+        text = text.strip()
+        if text == '>':
+            self.old_text = True
+            self.has_newline = True
+            return ''
+        if not text:
+            self.has_newline = True
+        if text:
+            if self.has_newline:
+                self.has_newline = False
+        return text
+
     def tag(self, html):
         return ("LOLOL")
 
     def image(self, src, title, alt_text):
-        print(src)
-        import requests
-        from fabulous import image
-        from io import BytesIO
-        r = requests.get(src)
-        if r.status_code != 200:
-            print(self.term.red("Cannot fetch %s" % src))
-        else:
-            open('test.jpg', 'wb').write(r.content)
-            for line in image.Image('test.jpg', width = 80):
-                print(line)
-        return "[IMAGE %s (%s)]" % (title or alt_text or '', src)
+        try:
+            r = requests.get(src.replace('\n', ''))
+            if r.status_code != 200:
+                print(self.term.red("Cannot fetch %s" % src))
+            else:
+                self.is_image = True
+                return '\n'.join(image.Image(BytesIO(r.content), width = 80))
+        except:
+            return "[IMAGE %s (%s)]" % (title or alt_text or '', src)
 
     def double_emphasis(self, text):
         return self.term.bold(text)
@@ -118,12 +149,12 @@ def print_text(text):
         if paragraph.startswith('>'):
             pass
 
-        lines = textwrap.wrap(paragraph, width = 79)
+        lines = textwrap.wrap(paragraph, width = 72)
         for line in lines[:-1]:
             words = line.split()
             spaces = (len(words) - 1) * [' ']
             words_length = sum(len(w) for w in words)
-            spaces_left = (80 - len(line))
+            spaces_left = (72 - len(line))
             word_weights = [
                 (len(word), pos) for pos, word in enumerate(words[1:])
             ]
@@ -175,14 +206,19 @@ def show_mail(curs, mail, markdown):
         print(" --- empty message ---")
 
     curs.execute(
-        "SELECT content_type, headers FROM binary_content "\
+        "SELECT content_type, headers, payload FROM binary_content "\
         "WHERE mail_id = ?",
         (mail.id, )
     )
 
     for row in curs.fetchall():
+        content_type = row[0]
         print("CONTENT:", row[0])
-    print('-' * 79)
+        if content_type.startswith('image/'):
+            for line in image.Image(BytesIO(row[2])):
+                print(line)
+            pass
+        print('-' * 79)
 
 
 
