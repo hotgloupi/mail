a8bcf6ae86e4853ddaffb4eb0c4ca3fe59682890
Raphaël Londeix <raphael.londeix@celoxica.com>
2014-09-16 09:38:22 +0200

    das

--- src/mail/commands/list.py
+++ src/mail/commands/list.py
@@ -4,11 +4,18 @@ from mail import db, account, client, message, pager, format
 
 def parse_args(args):
     parser = argparse.ArgumentParser(prog = 'mail-fetch')
+    parser.add_argument(
+        '-n',
+        dest = 'count',
+        help = 'Number of mail to display',
+        default = 30,
+        type = int,
+    )
     return parser.parse_args(args)
 
 def run(args):
     conn = db.conn()
-    for msg in message.fetch(conn, count = 20):
+    for msg in message.fetch(conn, count = args.count):
         subject = msg.subject.replace('\r', ' ').replace('\n', ' ').replace('\t', ' ')
         while '  ' in subject:
             subject = subject.replace('  ', ' ')
